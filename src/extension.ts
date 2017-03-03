6374c92e791029057bc4808129afd698a230357d
Sashko Stubailo <sashko@stubailo.com>
2017-03-03 14:17:21 -0800

    Reformat code and first working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,68 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { workspace, commands, ExtensionContext, window } from 'vscode';
+import { dirname } from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ignore-gitignore" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "ignore-gitignore" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = commands.registerCommand('extension.sayHello', async () => {
+    // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    // Just use the root one for now
+    const gitignores = await workspace.findFiles('**/.gitignore')
+
+    let path;
+    if (!gitignores.length) {
+      window.showInformationMessage('No .gitignore files found in current workspace.');
+      return;
+    }
+
+    const excludeObj: { [glob: string]: boolean } = {};
+    const gitignoreDocumentPromises = gitignores.map((file) => {
+      return workspace.openTextDocument(file.path);
     });
 
-    context.subscriptions.push(disposable);
+    const gitignoreDocuments = await Promise.all(gitignoreDocumentPromises);
+
+    gitignoreDocuments.forEach((doc) => {
+      const relativePath = workspace.asRelativePath(doc.fileName);
+      const dir = dirname(relativePath);
+
+      // Let's do search for now
+      for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
+        const lineText = doc.lineAt(lineIndex).text;
+
+        if (lineText.length) {
+          let lineTextWithPath = lineText;
+          if (dir !== '.') {
+            lineTextWithPath = dir + '/' + lineText;
+          }
+
+          excludeObj[lineTextWithPath] = true;
+        }
+      }
+    })
+
+    const config = await workspace.getConfiguration();
+    config.update('search.exclude', excludeObj, false);
+    config.update('files.exclude', excludeObj, false);
+
+    window.showInformationMessage(
+      `Successfully overwrote search.exclude and files.exclude from \
+${gitignoreDocuments.length} .gitignore files.`);
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
